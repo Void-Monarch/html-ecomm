8828581731784450005c8e8f2332ca45587c00b5
VoidMonarch <102967317+Void-Monarch@users.noreply.github.com>
2023-07-14 14:22:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const nav = document.querySelector('.nav');
@@ -165,3 +162,33 @@ const slider = function () {
   };
   slider();
   
+
+
+// Modal window login
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelector('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+
+btnsOpenModal.addEventListener('click', openModal );
+
+btnCloseModal.addEventListener('click', closeModal );
+overlay.addEventListener('click', closeModal );
+
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden'))  closeModal()
+});
